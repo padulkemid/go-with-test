08a3f9567ebd671de462954345c98c0070744545
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2024-04-07 04:34:50 +0700

    "learn-go-with-test refactor test hello"

--- hello.go
+++ hello.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
-func Hello(name string) string  {
-  return "Hello, " + name
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return englishHelloPrefix + name
 }
 
-func main()  {
-  fmt.Println(Hello("world"))
+func main() {
+	fmt.Println(Hello("world"))
 }

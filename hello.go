608d4c821c555db4dac6e377d5354bdd14e9422c
Fadhil Muhammad <fadhil.setiawan@tech.jago.com>
2024-04-07 15:26:08 +0700

    "learn-go-with-test hello world finished"

--- hello.go
+++ hello.go
@@ -2,16 +2,40 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
 
-func Hello(name string) string {
-	if name == "" {
-		name = "World"
+	spanish = "Spanish"
+	french  = "French"
+
+	defaultNameSuffix = "World"
+)
+
+func greetingPrefix(lang string) string {
+	var prefix string
+
+	switch lang {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
 
-	return englishHelloPrefix + name
+	return prefix
+}
+
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = defaultNameSuffix
+	}
+
+	return greetingPrefix(lang) + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
